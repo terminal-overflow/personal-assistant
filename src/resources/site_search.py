d96379c8692cd3f76ea001d03172527d80aeba72
terminal-flow <68205939+terminal-flow@users.noreply.github.com>
2021-07-10 14:22:28 +0100

    updated format of search

--- src/resources/site_search.py
+++ src/resources/site_search.py
@@ -1,22 +1,22 @@
 import webbrowser
 
 def get_youtube():
-    url = 'https://www.youtube.com'
+    url = 'https://youtube.com'
     webbrowser.open_new(url= url)
     return 'opening youtube'
 
 def search_youtube(terms):
-    url = f'https://www.youtube.com/results?search_query={terms}'
+    url = f'https://youtube.com/results?search_query={terms}'
     webbrowser.open_new(url= url)
     return 'searching youtube'
 
 def search_browser(terms):
-    url = f'https://www.{terms}'
+    url = f'https://{terms}'
     webbrowser.open_new(url= url)
     return 'searching'
 
 def get_duckduckgo():
-    url = 'https://www.duckduckgo.com'
+    url = 'https://duckduckgo.com'
     webbrowser.open_new(url= url)
     return 'opening search engine'
 
@@ -26,21 +26,21 @@ def search_duckduckgo(terms):
     return 'searching'
 
 def get_google():
-    url = 'https://www.google.com'
+    url = 'https://google.com'
     webbrowser.open_new(url= url)
     return 'opening search engine'
 
 def search_google(terms):
-    url = f'https://www.google.com/search?q={terms}'
+    url = f'https://google.com/search?q={terms}'
     webbrowser.open_new(url= url)
     return 'searching'
 
 def get_bing():
-    url = 'https://www.bing.com'
+    url = 'https://bing.com'
     webbrowser.open_new(url= url)
     return 'opening search engine'
 
 def search_bing(terms):
-    url = f'https://www.bing.com/search?q={terms}'
+    url = f'https://bing.com/search?q={terms}'
     webbrowser.open_new(url= url)
     return 'searching'
